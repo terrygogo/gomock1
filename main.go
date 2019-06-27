6c16b562b7b385037bb5a323a0301efdb09ecece
terrygogo <terrykimgo@gmail.com>
2019-06-27 17:07:58 +0900

    save src

--- main.go
+++ main.go
@@ -3,18 +3,76 @@ package main
 import (
 	"./server"
 	"context"
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
+
+	//"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// Trainer sdsd
+type Trainer struct {
+	ID struct {
+		Oid string `json:"$oid"`
+	} `json:"_id"`
+	Item   string `json:"item"`
+	Qty    int    `json:"qty"`
+	Status string `json:"status"`
+	Size   struct {
+		H   int    `json:"h"`
+		W   int    `json:"w"`
+		Uom string `json:"uom"`
+	} `json:"size"`
+	Tags []string `json:"tags"`
+}
+
+// Logger ddd
+//func Logger() http.Handler {
+//    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//        fmt.Println(time.Now(), r.Method, r.URL)
+//        router.ServeHTTP(w, r) // dispatch the request
+//    })
+//}
 
 func main() {
 	// This is the domain the server should accept connections for.
 
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+collection := client.Database("test").Collection("inventory")
+// create a value into which the result can be decoded
+var result Trainer
+
+err = collection.Find(context.TODO(), filter).Decode(&result)
+if err != nil {
+    log.Fatal(err)
+}
+
+fmt.Printf("Found a single document: %+v\n", result)
 	handler := server.NewRouter()
 	// http.ListenAndServe( ":443", handler)
- 
+
 	srv := &http.Server{
 		Addr:         ":8080",
 		Handler:      handler,
@@ -23,10 +81,8 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 
-
 	go func() {
 		srv.ListenAndServe()
-	
 
 	}()
 
